5626f1e68bb08e3c5f4e1bc4acebf2e1ac3b926f
Mem0Real <juniourmimo@gmail.com>
2023-01-03 15:44:18 +0300

    Still tryna fix the footer font

--- navigation/Footer.js
+++ navigation/Footer.js
@@ -7,7 +7,7 @@ const footer = () => {
     <footer className="text-gray-400 bg-stone-900 body-font mt-12 md:mt-0 flex flex-col md:flex-row">
       <div className="container px-5 py-5 flex justify-start items-center flex-col md:flex-row basis-2/3">
         <a className="flex title-font font-medium items-center justify-center text-white border border-y-0 rounded-md border-r-0 border-l-0 mr-2">
-          <span className="mx-5 text-xl">AbuSolarTech</span>
+          <p className="mx-5 text-xl">AbuSolarTech</p>
         </a>
         {/* <div className="hidden md:block border border-y-0 border-l-0 h-7 mx-5"></div> */}
         <p className="text-sm text-gray-400 pl-2 sm:ml-4 sm:pl-4 sm:border-l-2 mt-2 md:mt-0 sm:border-gray-800 sm:py-2 sm:mt-0">
